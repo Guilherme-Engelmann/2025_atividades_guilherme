c5265393551aa3e179b7e18e4c34b1ab30b65836
Guilherme Engelmann <guilherme_engelmann@estudante.sesisenai.org.br>
2025-10-30 09:03:52 -0300

    add

--- atividade_crud/api/update_task.php
+++ atividade_crud/api/update_task.php
@@ -6,7 +6,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 if(!isset($data['id_tarefa'])){
     http_response_code(400);
     echo json_encode(['error'=>'ID da tarefa ausente']);
-    exit;
+    exit();
 }
 $fields = [];
 $params = [];
@@ -24,11 +24,11 @@ if(isset($data['descricao'])){
 if(!$fields){
     http_response_code(400);
     echo json_encode(['error'=>'Nada para atualizar']);
-    exit;
+    exit();
 }
 $params[] = $data['id_tarefa'];
 $types .= 'i';
-$sql = 'UPDATE tarefas SET '.implode(',', $fields).' WHERE id_tarefa=?';
+$sql = 'UPDATE tarefas SET '.implode(', ', $fields).' WHERE id_tarefa=?';
 $stmt = $conn->prepare($sql);
 $stmt->bind_param($types, ...$params);
 if($stmt->execute()){
